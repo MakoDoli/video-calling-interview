080162cd3258a8a0472bf4a9d67ceda00b01b8cd
MakoDoli <makodolidze@hotmail.com>
2025-06-18 23:22:19 +0400

    add clerk, convex and stream sdks

---
+++ src/components/providers/ConvexClerkProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { useAuth } from "@clerk/nextjs";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+function ConvexClerkProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      {children}
+    </ConvexProviderWithClerk>
+  );
+}
+
+export default ConvexClerkProvider;
