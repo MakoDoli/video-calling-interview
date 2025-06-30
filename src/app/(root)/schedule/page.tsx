29c66812a70a33909b70579a7bc28123f85ed868
MakoDoli <makodolidze@hotmail.com>
2025-06-30 18:16:00 +0400

    add schedule page

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+import LoaderUI from "@/components/LoaderUI";
+import { useUserRole } from "@/hooks/useUserRole";
+import { useRouter } from "next/navigation";
 import React from "react";
+import InterviewScheduleUI from "./InterviewScheduleUI";
 
 export default function SchedulePage() {
-  return <div>page</div>;
+  const router = useRouter();
+
+  const { isInterviewer, isLoading } = useUserRole();
+  if (isLoading) return <LoaderUI />;
+  if (!isInterviewer) router.push("/");
+  return <InterviewScheduleUI />;
 }
